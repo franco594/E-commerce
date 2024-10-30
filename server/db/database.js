6c64842ef273ae70af5010706534596921a27ac7
Franco Pieroni <71734317+franco594@users.noreply.github.com>
2024-10-30 03:17:11 -0300

    Modificaciones .env y database.js

--- server/db/database.js
+++ server/db/database.js
@@ -1,36 +1,15 @@
 // database.js
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-import mysql from 'mysql2/promise'; // Importa mysql2 para crear la base de datos
 
 // Carga las variables de entorno
 dotenv.config();
 
-// Función para crear la base de datos si no existe
-const createDatabaseIfNotExists = async () => {
-  const connection = await mysql.createConnection({ 
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD
-  });
-
-  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
-  await connection.end();
-};
-
-// Llama a la función antes de crear la instancia de Sequelize
-await createDatabaseIfNotExists();
-
-//const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//  host: process.env.HOST,
-//  dialect: 'mysql',
-//  logging: false // Desactivar el logging
-//});
-
+// Configura la conexión a la base de datos usando DATABASE_URL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   dialectOptions: {
-    connectTimeout: 30000, // Increase timeout to 30 seconds
+    connectTimeout: 30000, // Aumenta el tiempo de espera a 30 segundos
     ssl: {
       require: true,
       rejectUnauthorized: false // Permite conexiones SSL
@@ -39,15 +18,11 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false // Desactiva logging
 });
 
+// Prueba la conexión a la base de datos
 sequelize.authenticate()
   .then(() => console.log('Conectado exitosamente a la base de datos en Railway'))
   .catch(err => console.error('No se pudo conectar a la base de datos:', err));
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
+// Exporta la instancia de Sequelize
 export default sequelize;
+
